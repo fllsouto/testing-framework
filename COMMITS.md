d751ebfe98a90d291d625ce89f6a5f4cda1c65f6
Fellipe Souto <fllsouto@gmail.com>
2026-04-12 21:47:12 -0300

    [STEP] Adding xtest_result. Breaking test_xtest_template_method() method intentionally.

--- COMMITS.md
+++ COMMITS.md
@@ -2,6 +2,10 @@
 
 ## Commit 1 - [1. Setup]
 ## Commit 2 - [2. Classe XTestCase]
+
+- https://github.com/fllsouto/testing-framework/commit/1b7748407760b87f1ebfdc943b36daf0acaec5e4
+- https://github.com/fllsouto/testing-framework/actions/runs/24318945234
+
 ## Commit 3 - [3. Classe TestResult]
 ## Commit 4 - [4. Testando TestCase]
 ## Commit 5 - [5. Classe TestSuite]
--- src/app/xtest_case.py
+++ src/app/xtest_case.py
@@ -1,12 +1,21 @@
+from app.xtest_result import XTestResult
+
+
 class XTestCase:
 
     def __init__(self, test_method_name: str):
         self.test_method_name = test_method_name
 
-    def run(self):
+    def run(self, result: XTestResult):
+        result.test_started()
         self.setup()
-        test_method = getattr(self, self.test_method_name)
-        test_method()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError as e:
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            result.add_error(self.test_method_name)
         self.tear_down()
 
     def setup(self):
